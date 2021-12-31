6d98f831fba1bf2933dc869b7e9fdbe7bec20a8c
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-31 14:54:47 -0500

    update automation scripts

--- automate/download_summaries.sh
+++ automate/download_summaries.sh
@@ -5,6 +5,5 @@
 for s in bookwolf cliffnotes gradesaver novelguild pinkmonkey shmoop sparknotes thebestnotes
 do
    cd ~/booksum/scripts/data_collection/$s/
-   ls
-#   python3 get_summaries.py &
+   python3 get_summaries.py &
 done
