bac36530dcdf7618c8f5b7404ea2bc93b2480ceb
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-31 14:38:58 -0500

    automate

--- automate/download_summaries.sh
+++ automate/download_summaries.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
 cd ~/booksum/scripts/data_collection/$1/
+echo ls
--- automate/init.sh
+++ automate/init.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 cd ~/booksum
 gsutil cp gs://sfr-books-dataset-chapters-research/all_chapterized_books.zip .
