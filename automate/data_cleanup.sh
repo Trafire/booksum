b43fb79cc7617f96a41e752b9b52a1281216319a
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-31 22:01:28 -0500

    update automation scripts

--- automate/data_cleanup.sh
+++ automate/data_cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/booksum/scripts/data_cleaning_scripts/
+cd ~/booksum/scripts/data_cleaning/
 python3 basic_clean.py
 python3 split_aggregate_chaps_all_sources.py
 python3 clean_summaries.py
