411fef2c0d15e0682c7f551e5b8146676328b092
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2022-01-03 12:04:41 -0500

    update get summary

--- scripts/data_cleaning/basic_clean.py
+++ scripts/data_cleaning/basic_clean.py
@@ -7,15 +7,15 @@
  */
 """
 
-
 # Also converts files from unicode_cleaned directory by adding part_0.txt to all the files before splitting
 
 import json
 import os
 import re
+
 from unidecode import unidecode
 
-sources = ['gradesaver', 'shmoop',  'cliffnotes', 'sparknotes', 'pinkmonkey', 'bookwolf',  'novelguide', 'thebestnotes']
+sources = ['gradesaver', 'shmoop', 'cliffnotes', 'sparknotes', 'pinkmonkey', 'bookwolf', 'novelguide', 'thebestnotes']
 
 BASE_DIR = "../raw_summaries/"
 
@@ -23,59 +23,62 @@ f_errors = open("summary_not_found.txt", "w")
 
 for src in sources:
 
-    print ("src: ", src)
+    print("src: ", src)
 
     books_dir = os.path.join(BASE_DIR, src, "summaries")
-    dest_dir = os.path.join("../cleaning_phase" , src)
+    dest_dir = os.path.join("../cleaning_phase", src)
 
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
+    try:
+        for book in os.listdir(books_dir):
+            try:
 
-    for book in os.listdir(books_dir):
-        try:
+                book_path = os.path.join(books_dir, book)
+                print(book, "===")
 
-            book_path = os.path.join(books_dir, book)
-            print (book, "===")
+                book_dest = os.path.join(dest_dir, unidecode(book))
+                if not os.path.exists(book_dest):
+                    os.makedirs(book_dest)
 
-            book_dest = os.path.join(dest_dir, unidecode(book))
-            if not os.path.exists(book_dest):
-                os.makedirs(book_dest)
+                for section in os.listdir(book_path):
 
-            for section in os.listdir(book_path):
+                    summary_path = os.path.join(book_path, section)
 
-                summary_path = os.path.join(book_path, section)
+                    fp = open(summary_path, "r")
 
-                fp = open(summary_path,"r")
+                    try:
+                        summary_json = json.loads(fp.readlines()[0])
+                    except Exception as e:
+                        print(book, "=Error reading json==", e, section)
+                        f_errors.write(summary_path + str(e) + "\n")
+                        continue
 
-                try:
-                    summary_json = json.loads(fp.readlines()[0])
-                except Exception as e:
-                    print (book, "=Error reading json==", e, section)
-                    f_errors.write(summary_path + str(e) + "\n")
-                    continue
+                    # Remove text inside paranthesis
 
-                #Remove text inside paranthesis
+                    if type(summary_json['summary']) is list:
+                        summary_json['summary'] = " ".join(summary_json['summary'])
+                    summary_json['summary'] = unidecode(summary_json['summary'])
+                    summary_json['summary'] = re.sub("[\(\[].*?[\)\]]", "", summary_json['summary'])
 
-                if type(summary_json['summary']) is list:
-                    summary_json['summary'] = " ".join(summary_json['summary'])
-                summary_json['summary'] = unidecode(summary_json['summary'])
-                summary_json['summary'] = re.sub("[\(\[].*?[\)\]]", "", summary_json['summary'])
+                    if 'analysis' in summary_json and summary_json['analysis'] is not None:
+                        if type(summary_json['analysis']) is list:
+                            summary_json['analysis'] = " ".join(summary_json['analysis'])
+                        summary_json['analysis'] = unidecode(summary_json['analysis'])
+                        summary_json['analysis'] = re.sub("[\(\[].*?[\)\]]", "", summary_json['analysis'])
 
-                if 'analysis' in summary_json and summary_json['analysis'] is not None:
-                    if type(summary_json['analysis']) is list:
-                        summary_json['analysis'] = " ".join(summary_json['analysis'])
-                    summary_json['analysis'] = unidecode(summary_json['analysis'])
-                    summary_json['analysis'] = re.sub("[\(\[].*?[\)\]]", "", summary_json['analysis'])
+                    # Section name remains the same for overview.txt
+                    section_out = section
 
-                # Section name remains the same for overview.txt
-                section_out = section
+                    if '.txt' in section and 'overview' not in section:
+                        section_out = section[
+                                      0:-4] + "_part_0.txt"  # add _part_0 to every summary file for splitting operations later
 
-                if '.txt' in section and 'overview' not in section:
-                    section_out = section[0:-4] + "_part_0.txt" # add _part_0 to every summary file for splitting operations later
+                    book_dest_path = os.path.join(book_dest, section_out)
 
-                book_dest_path = os.path.join(book_dest, section_out)
-
-                with open(book_dest_path, 'w') as outfile:
-                    json.dump(summary_json, outfile)
-        except:
-            pass
+                    with open(book_dest_path, 'w') as outfile:
+                        json.dump(summary_json, outfile)
+            except:
+                pass
+    except:
+        pass
